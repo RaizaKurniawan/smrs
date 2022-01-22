b1777d96bb5090acf4d610e3b03ad6fcd89ba868
RaizaKurniawan <raizakurniawan@gmail.com>
2022-01-22 22:51:18 +0700

    second

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+
+ const signUpButton = document.getElementById('signUp');
+ const signInButton = document.getElementById('signIn');
+ const container = document.getElementById('container');
+ 
+ signUpButton.addEventListener('click', () => {
+     container.classList.add("right-panel-active");
+ });
+ 
+ signInButton.addEventListener('click', () => {
+     container.classList.remove("right-panel-active");
+ });
+
+
 require('./bootstrap');
+
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+require('./components/Example');
